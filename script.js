8c6a4d4fb2ced7ecb07943a28626fcace6420afc
Clazariuk1 <Clazariuk@gmail.com>
2023-11-28 19:42:53 -0800

    updated pseudocode as separate text file and uploaded wireframe images

--- script.js
+++ script.js
@@ -108,6 +108,27 @@ function wallCollision(top, left, right, bottom) {
   return false;
 }
 
+// ENEMY COLLISION DETECTION BELOW. Currently must refactor for my script.
+function enemyCollision(top, left, right, bottom) {
+  const charRect = charDiv.getBoundingClientRect();
+  const enemyRect = enemy.getBoundingClientRect();
+  const scrollTop = document.documentElement.scrollLeft;
+  const scrollLeft = document.documentElement.scrollTop;
+  const charTop = charRect.top + scrollTop;
+  const charLeft = charRect.left + scrollLeft;
+  const enemyTop = enemyRect.top + scrollTop;
+  const enemyLeft = enemyRect.left + scrollLeft;
+
+  const overlapEnemyX =
+    charLeft + left < enemyRect.right &&
+    charLeft + charDiv.offsetWidth + right > enemyRect.left;
+  const overlapEnemyY =
+    charTop + top < enemyRect.bottom &&
+    charTop + charDiv.offsetHeight + bottom > enemyRect.top;
+  return overlapEnemyX && overlapEnemyY && hasSword;
+}
+
+
 // KEY COMMITMENTS FOR PLAYER ACTIONS ..
 document.addEventListener('keydown', function(event) {
   if (event.key === 'e') {
@@ -136,6 +157,7 @@ const pauseMenuBtn = document.getElementById('pause-menu-button');
 const instBtn = document.getElementById('instructions-button');
 const muteMusicBtn = document.getElementById('mute-music');
 const muteSFXBtn = document.getElementById('mute-sfx');
+
 //Element Variables
 const instBox = document.getElementById('instructions-box')
 const pauseMenu = document.getElementById('pause-menu');
@@ -467,79 +489,3 @@ const levelBossWave = setTimeout(generateBossEnemies((this.level/3)), 3000);
 const levelWave = setTimeout(generateEnemies(this.level), 5000);
 const speedDash = setTimeout(revert, 5000);
 const cooldown = setTimeout(recharge, 5000);
-
-
-// // BUILD THE BASICS BEFORE YOU BUILD THE BONUSES!!!
-
-// "Dungeon Arena."
-// a top down action-rpg survival arcade game.
-// premise: you're an adventurer/explorer surviving waves of enemies in various environmentally themed levels.
-//    BONUS : the level set repeats at a higher difficulty Mario bros style, 1-2, 3-1, etc. enemy quantity+ .
-
-// Dungeon Arena function list
-// Character selection - three simple classes to choose from at the initial startup screen. fighter, thief, mage
-// Features a level up system that augments stats. HP, defense, attack, MP
-// special move added per level. Unlock a 'new ability', which would be keydown hot-keyed to the system and conditional unlock as able. spacebar basic attack. Special level one 'Q' special level two 'E'. Special level three 'R'. Start with thief class because they're cooler and the jaunt dash sounds awesome.
-// Lvl 0: Dash - unlock a jaunt dash
-// STEP. 1: keyframe animation to move player in direction of last keydown/movement
-//        2: Ensure hit box follows.
-// lvl 5: back-stab - a high critical strike that has chance to kill on strike.
-//         1: a damage x2 attack with random.Math on kill strike.
-// lvl 10: Mug - get bonus score points on successful strike.
-//         STEP: extra XP = math.random double bonus per enemy
-
-// Lvl 0: Cleave - strong fighter area of effect attack conic in front. Can you get it right?
-// lvl 5: IronSkin - Consume mana to block all attacks from doing damage.
-// lvl 10: Drain - attacks heal player for same amount they hit enemy.
-// Lvl 0: Restore - Mage heals with mana.
-// lvl 5: Fireball - straight line fire blast no penetration.
-// lvl 10: lightning bolt - straight line area of effect attack. Penetrates enemies/pixels
-
-//All characters:
-// Attack
-// Defend
-// CLOTHING ITEM OBJECTS THAT INCREASE YOUR LEVEL STATS --> Bonus level two.
-// Special attacks consume mana
-// Arrow key character icon movement -> critical to do with collision detection in tandem.
-// Enemies continually pour in from random parts of the screen edge to attack/converge on the player.
-// make player and enemies simple images trapped in boxes as icons.
-// add 'bounce' hover effect to characters.
-// Basic text boxes on event BONUS ! -> on critical strikes return a text box that says "'Take that!'"
-// All objects contain ‘image’ constructor that has image file location.
-// Start screen / Death screen. Final Score
-// BONUS : audio work. On special, on attack hear slash, bite noise on collision damage for player and death moan for zero hp.  Song on throughout. Adjustable volume slider for music and SFX separately.
-// Volume music slider!
-// Game drop down menu that contains stuff -> PAUSE MID-GAME menu. add whatever down the line
-// A saving system? Not yet, only in due time.
-// Collision detection an absolute must.
-
-
-// Skeleton monster boss
-// Ferocious Bear boss
-// The Captain of the Guard.
-// Ogre / troll / Thug
-// Dungeon rat / wild hound / woodsman
-// Crypt robber / Assassin / City Guard
-// Kill three things unleash a boss thing
-// LEVEL SCREENS
-// -dungeon; forest; city,
-
-// 0. Make a base layout with volume sliders top right, hp/mana bars top left.
-// 0.1 Player shape start center of space. --> how do we bind the avatar shape with the stats object for this?
-// 0.2 Give it the ability to move on keydown WASD or arrow keys
-// 0.3 make an 'enemy' block.
-// 0.4 Build an on collision detection event - damage.
-// 0.5 make the enemies continually chase after the player to touch and do damage.
-// 0.6 build level up system to increase the players strength and defense. The point is to get a level up quick, so make it easy like a kill or two.
-// 0.7 Point / score system. Enemies killed, levels survived, health lost. enemies multiplied by levels subtracted by health lost.
-// 0.8 make a PAUSE GAME SCREEN for
-// .9 layer on images for players / enemies to make simple hit box wrapped in pic.
-// Over Nine Thousand. Make all characters, not just the one.
-
-// 1. Build character class. HP / MP / STR / DEF / LEVEL
-// 2. Build enemy class. HP / STR / DEF / LEVEL
-// 3. Build subclasses for character type thief and enemy type dungeon rat
-// 4. Build random stat generator for the enemies' creation. Add in a condition of every 3 normal enemies, throw in a hard enemy. Make each boss a level.
-// 5. Build basic pixel-grid based attack for characters and enemies.
-//     -Specify the space being affected, the on success effects. Enemies instead must on-collision conditional.
-// 6. Build score system and display. Small Enemies killed * Big Enemies killed Level ?
